2bf80f49cec2df89c041d9e4b70f0bc598c2d7ce
Abolfazl-Azadegan <Abolfazl.azadegan@yahoo.com>
2026-09-19 19:53:53 +0330

    working with lifetime

---
+++ rust/src/bin/00_training.rs
@@ -0,0 +1,20 @@
+
+
+
+fn largest_value (list: &[i32]) -> i32{
+    let mut largest = list[0];
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+
+fn main(){
+
+
+
+
+}
